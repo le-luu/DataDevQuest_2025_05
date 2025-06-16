1b859639d159b606c34d0377031c1361388a8fc7
Le Luu <le.luu.dev@gmail.com>
2025-06-16 07:45:28 -0400

    Update config.py

--- DDQ_2025_05_Intermediate/config.py
+++ DDQ_2025_05_Intermediate/config.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 
 #Change your Path where you cloned your DataDevQuest_2025_05
-parent_path = Path(r"C:\Users\LeLuu\Documents\TableauDevQuest\DataDevQuest_Challenges\2025_05")
+parent_path = Path(r"CHANGE YOUR REPO DIRECTORY HERE")
